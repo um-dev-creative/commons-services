bc97734f7b51b8033e65fb252f00502cd4fb7c43
Luis Mata <69470188+lanmata@users.noreply.github.com>
2025-12-19 01:05:21 -0500

    Upgrade commons-services to Spring Boot 4.x and Java 21

---
+++ src/main/java/com/umdev/commons/services/CrudService.java
@@ -0,0 +1,69 @@
+/*
+ *  @(#)CrudService.java
+ *
+ *  Copyright (c) Luis Antonio Mata Mata. All rights reserved.
+ *
+ *   All rights to this product are owned by Luis Antonio Mata Mata and may only
+ *  be used under the terms of its associated license document. You may NOT
+ *  copy, modify, sublicense, or distribute this source file or portions of
+ *  it unless previously authorized in writing by Luis Antonio Mata Mata.
+ *  In any event, this notice and the above copyright must always be included
+ *  verbatim with this file.
+ */
+
+package com.umdev.commons.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+/// Interface for CRUD operations.
+/// Provides default methods for creating, updating, deleting, finding, and listing entities.
+///
+/// @param <T> the type of the entity
+/// @version 1.0.0, 20-10-2020
+public interface CrudService<A, T> {
+
+    /// Creates a new entity.
+    ///
+    /// @param t the entity to create
+    /// @return the created entity wrapped in a ResponseEntity
+    default ResponseEntity<T> create(T t) {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    /// Updates an existing entity.
+    ///
+    /// @param id the ID of the entity to update
+    /// @param t the entity with updated information
+    /// @return the updated entity wrapped in a ResponseEntity
+    default ResponseEntity<T> update(A id, T t) {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    /// Deletes an entity.
+    ///
+    /// @param id the ID of the entity to delete
+    /// @param t the entity to delete
+    /// @return the deleted entity wrapped in a ResponseEntity
+    default ResponseEntity<T> delete(A id, T t) {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    /// Finds an entity by its ID.
+    ///
+    /// @param id the ID of the entity to find
+    /// @return the found entity wrapped in a ResponseEntity
+    default ResponseEntity<T> find(A id) {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    /// Lists entities by their IDs.
+    ///
+    /// @param id the IDs of the entities to list
+    /// @return the list of entities wrapped in a ResponseEntity
+    default ResponseEntity<List<T>> list(A... id) {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+}
---
+++ src/main/java/com/umdev/commons/services/loggers/interceptor/LogInterceptor.java
@@ -0,0 +1,56 @@
+/*
+ *  @(#)LogInterceptor.java
+ *
+ *  Copyright (c) Luis Antonio Mata Mata. All rights reserved.
+ *
+ *   All rights to this product are owned by Luis Antonio Mata Mata and may only
+ *  be used under the terms of its associated license document. You may NOT
+ *  copy, modify, sublicense, or distribute this source file or portions of
+ *  it unless previously authorized in writing by Luis Antonio Mata Mata.
+ *  In any event, this notice and the above copyright must always be included
+ *  verbatim with this file.
+ */
+
+package com.umdev.commons.services.loggers.interceptor;
+
+import com.umdev.commons.services.loggers.LoggingService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/// InterceptorLog.
+///
+/// @author Luis Antonio Mata
+/// @version 0.0.1
+/// @since 21
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+
+    private final LoggingService loggingService;
+
+    ///  Constructor.
+    public LogInterceptor(LoggingService loggingService) {
+        this.loggingService = loggingService;
+    }
+
+    ///  PreHandle. Method that is executed before the request is processed.
+    /// @param request HttpServletRequest
+    /// @param response HttpServletResponse
+    /// @param handler Object
+    /// @return boolean
+    /// @throws Exception Exception
+    /// @since 21
+    @Override
+    public boolean preHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws Exception {
+        if (request.getMethod().equals(HttpMethod.GET.name())
+                ||request.getMethod().equals(HttpMethod.POST.name())
+                ||request.getMethod().equals(HttpMethod.DELETE.name())
+                ||request.getMethod().equals(HttpMethod.PUT.name())) {
+            loggingService.displayRequest(request, null);
+        }
+        return true;
+    }
+}
